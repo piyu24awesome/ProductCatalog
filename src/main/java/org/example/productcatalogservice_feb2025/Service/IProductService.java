43b7374ddb17b07a21de6d0e5e1d9a65612a3310
piyu24awesome <84369564+piyu24awesome@users.noreply.github.com>
2025-02-25 08:47:11 +0530

    added product controller

---
+++ src/main/java/org/example/productcatalogservice_feb2025/Service/IProductService.java
@@ -0,0 +1,8 @@
+package org.example.productcatalogservice_feb2025.Service;
+
+import org.example.productcatalogservice_feb2025.models.Product;
+
+public interface IProductService {
+
+    public Product getProductById(long id);
+}
